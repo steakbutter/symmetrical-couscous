2b3b06326cc3f842a59dcb756b6c958ac33e5bf6
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-07-02 19:09:02 -0600

    remove transaction form from login page

---
+++ Public/js/data.js
@@ -0,0 +1,10 @@
+// let totalIncome = 0;
+// let totalExpenses = 0;
+
+// user.transactions.forEach(transaction => {
+//   if (transaction.type === 'Income') {
+//     totalIncome += transaction.amount;
+//   } else if (transaction.type === 'Expenses') {
+//     totalExpenses += transaction.amount;
+//   }
+// });
